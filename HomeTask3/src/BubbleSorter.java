33e589de72f17d160b7bb8813042570a74bafd24
Ramzes <deRamzes@gmail.com>
2015-05-04 22:42:49 +0300

    fix errors, and refactor

--- HomeTask3/src/BubbleSorter.java
+++ HomeTask3/src/BubbleSorter.java
@@ -1,10 +1,12 @@
 /**
  * Created by Ramzes on 03.05.2015.
  */
-public class BubbleSorter extends MainSorter{
+public class BubbleSorter extends MainSorter {
 
 
-	public BubbleSorter() {super();}
+	public BubbleSorter() {
+		super();
+	}
 
 	public BubbleSorter(double[] arrayToSort) {
 		setArrayToSort(arrayToSort);
@@ -13,11 +15,11 @@ public class BubbleSorter extends MainSorter{
 
 	public double[] sort() {
 
-		for (int x=0;x< arrayToSort.length-1;x++) {
+		for (int x = 0; x < arrayToSort.length - 1; x++) {
 
-			for (int z=0;z<arrayToSort.length-x-1;z++) {
-				if (arrayToSort[z]>arrayToSort[z+1]) {
-					swapArrayidx(z,z+1);
+			for (int z = 0; z < arrayToSort.length - x - 1; z++) {
+				if (arrayToSort[z] > arrayToSort[z + 1]) {
+					swapArrayidx(z, z + 1);
 				}
 			}
 		}
