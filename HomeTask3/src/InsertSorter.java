4f996ac790aec8711c64ac8538cfe22b73ec4e6d
Ramzes <deRamzes@gmail.com>
2015-05-05 00:27:57 +0300

    some comment

--- HomeTask3/src/InsertSorter.java
+++ HomeTask3/src/InsertSorter.java
@@ -10,10 +10,12 @@ public class InsertSorter extends MainSorter {
 		for (int i = 1; i < arrayToSort.length; i++) {
 			// get second  value of array
 			double value = arrayToSort[i];
-
+			//cycle and compare with prevision element
+			// if hey bigger, shift it
 			for ( j = i - 1; j >= 0 && arrayToSort[j] > value; j--) {
 				arrayToSort[j + 1] = arrayToSort[j];
 			}
+			//set min val
 			arrayToSort[j + 1] = value;
 		}
 	}
