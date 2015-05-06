4c9f1e6d50d6f14c10773b52b1aadf30a9310daa
ramzesik <deRamzes@gmail.com>
2015-05-06 13:52:25 +0300

    guess

--- HomeTask4/src/ReadDigit.java
+++ HomeTask4/src/ReadDigit.java
@@ -7,14 +7,13 @@ import javax.swing.*;
 
 public class ReadDigit {
 
-	private int message;
 
 	public int read() {
 
 		//get coding
 		//while (message == 0 ) {
 			//get string
-			message = Integer.parseInt(JOptionPane.showInputDialog("Please enter digh: "));
+		int message = Integer.parseInt(JOptionPane.showInputDialog("Please enter digh: "));
 		//	if (message == 0) {
 		//		break;
 		//	}
